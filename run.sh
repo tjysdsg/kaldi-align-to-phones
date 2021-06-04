1f16360673d54500ba435e18a3f772ae4cb3b7f8
唐际洋 <tangjiyang@xiaomi.com>
2021-06-04 17:24:38 +0800

    Add test scp files

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 set -e
 
+data=test
 root=/home/storage07/zhangjunbo/src/kaldi/egs/librispeech/s5
 mdl=$root/exp/nnet3_cleaned/tdnn_sp/final.mdl
 tree=$root/exp/nnet3_cleaned/tdnn_sp/tree
@@ -17,8 +18,8 @@ stage=0
 . ./path.sh
 . parse_options.sh
 
-cat ivector_dev_clean.scp ivector_dev_other.scp > ivector_online.scp
-cat feats_dev_clean.scp feats_dev_other.scp > feats.scp || exit 1
+cat ivector_${data}_clean.scp ivector_${data}_other.scp > ivector_online.scp
+cat feats_${data}_clean.scp feats_${data}_other.scp > feats.scp || exit 1
 
 ./nnet3-align-to-phones.sh --stage $stage \
     --mdl $mdl \
